a2556aaffe2df16736c18a8fc790f03ea0daacde
osemes1 <130790511+osemes1@users.noreply.github.com>
2023-09-28 09:41:45 +0300

    Commit DZ32 2023-09-28 09:40

---
+++ scr/main/java/Product.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double cost;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+}
