b2204486fff8ef244e5d721b691e1d606aa1566c
osemes1 <130790511+osemes1@users.noreply.github.com>
2023-09-28 09:41:01 +0300

    Commit DZ32 2023-09-28 09:40

---
+++ scr/main/java/Order.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+public class Order {
+    private Long id;
+    private Timestamp date;
+    private Double cost;
+    private List<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
